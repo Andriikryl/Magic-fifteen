e65fd1ab5fd522d4d4ae3fafda0278ead062b654
Amdrii <andreyk557@gmail.com>
2023-02-21 18:15:57 +0200

    feat[swap-func]

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ containerNode.addEventListener('click', (event) => {
     const isValid = isValidForSwap(buttonCords, blankCords);
 
     if(isValid){
-        
+        swap(blankCords, buttonCords, matrix)
+        setPositionItems(matrix)
     }
 })
 
@@ -97,10 +98,14 @@ function isValidForSwap(coords1, coords2){
     const diffX = Math.abs(coords1.x - coords2.x)
     const diffY = Math.abs(coords1.y - coords2.y)
 
-    return (diffX === 1 || diffY === 1) && (coords1.x === coords2.x || coords1.y === coords1.y)
+    return (diffX === 1 || diffY === 1) && (coords1.x === coords2.x || coords1.y === coords2.y)
 }
 
-
+function swap(coords1, coords2, matrix) {
+    const coords1Number = matrix[coords1.y][coords1.x];
+    matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x]
+    matrix[coords2.y][coords2.x] = coords1Number;
+}
 
 
 
